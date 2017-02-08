c5bc13dab4b10f422524fa132e3fcb7ba41427c6
Fabio Gomez <fabiogomez@us.ibm.com>
2017-02-08 11:34:05 -0600

    - Simplified Catalog config - Modularized ItemService URL requests

--- catalog/src/main/java/catalog/Application.java
+++ catalog/src/main/java/catalog/Application.java
@@ -15,15 +15,5 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
         System.out.println("Inventory microservice is ready for business...");
-
-        Config config = new Config();
-        String connection = config.es_url;
-
-        if (connection == null || connection.equals("")) {
-            System.out.println("Seems we don't have a connection string for Elasticsearch!");
-            System.out.println("Please provide Elasticsearch connection string in src/main/resources/application.yml OR");
-            System.out.println("Provide a value for environment variable \"elasticsearch_connection_string\"");
-            System.exit(-1);
-        }
     }
 }
