b9a56271d02407bb0c1d52af21008e1e3a3dddac
Fabio Gomez <fabiogomez@us.ibm.com>
2017-04-18 09:27:00 -0500

    Using deploy.sh script to get image name and secret and then deploy

--- Jenkinsfile
+++ Jenkinsfile
@@ -18,7 +18,7 @@ podTemplate(label: 'mypod',
                 sh 'cd catalog && ./gradlew build -x test'
             }
             stage ('Build Docker Image') {
-                sh 'cd catalog && ./gradlew docker && cd docker && docker build -t cloudnative/catalog-fabio-jenkins .'
+                sh 'cd catalog && ./gradlew docker && cd docker && docker build -t cloudnative/catalog-jenkins .'
             }
             stage ('Push Docker Image to Registry') {
                 sh """
@@ -38,20 +38,21 @@ podTemplate(label: 'mypod',
                 bx cs init
                 bx cr login
 
-                docker tag cloudnative/catalog-fabio-jenkins registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER}
-                docker push registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER}
+                docker tag cloudnative/catalog-jenkins registry.ng.bluemix.net/chrisking/catalog:${env.BUILD_NUMBER}
+                docker push registry.ng.bluemix.net/chrisking/catalog:${env.BUILD_NUMBER}
                 """
                 //sh "docker tag cloudnative/catalog-fabio-jenkins registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER}"
                 //sh "docker push registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER}"
             }
             stage ('Deploy to Kubernetes') {
-                sh """
+                /*sh """
                 #!/bin/bash
                 cd catalog
                 yaml w deployment.yml spec.template.spec.containers[0].image registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER} > deployment2.yml
                 kubectl --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` create -f deployment2.yml
                 kubectl --token=`cat /var/run/secrets/kubernetes.io/serviceaccount/token` create -f service.yml
-                """
+                """*/
+                sh 'cd catalog && ./deploy.sh'
                 //sh 'export KUBE_API_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`'
                 //sh "cd catalog && yaml w -i deployment.yml spec.template.spec.containers[0].image registry.ng.bluemix.net/chrisking/catalog-fabio-jenkins:${env.BUILD_NUMBER}"
                 //sh "cd catalog && kubectl --token=${KUBE_API_TOKEN} create -f deployment.yml"
--- catalog/deploy.sh
+++ catalog/deploy.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
 function get_elastic_secret {
-	echo $(kubectl get secrets | grep "compose-for-elasticsearch" | awk '{print $1}')
+	echo $(kubectl --token=${token} get secrets | grep "compose-for-elasticsearch" | awk '{print $1}')
 }
 
-echo "Checking cluster"
-IP_ADDR=$(bx cs workers $CLUSTER_NAME | grep deployed | awk '{ print $2 }')
-if [ -z $IP_ADDR ]; then
-  echo "$CLUSTER_NAME not created or workers not ready"
-  exit 1
-fi
-
-echo -e "Configuring vars"
-exp=$(bx cs cluster-config $CLUSTER_NAME | grep export)
-if [ $? -ne 0 ]; then
-  echo "Cluster $CLUSTER_NAME not created or not ready."
-  exit 1
-fi
-eval "$exp"
+token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+cluster_name=$(cat /var/run/secrets/bx-auth-secret/CLUSTER_NAME)
 
 # Check if elasticsearch secret exists
 elastic_secret=$(get_elastic_secret)
@@ -32,7 +20,7 @@ if [[ -z "${elastic_secret// }" ]]; then
 	fi
 
 	echo "Creating secret from ${elastic_service}"
-	bx cs cluster-service-bind $CLUSTER_NAME default $elastic_service
+	bx cs cluster-service-bind $cluster_name default $elastic_service
 
 	if [ $? -ne 0 ]; then
 	  echo "Could not create secret for ${elastic_service} service."
@@ -50,19 +38,20 @@ fi
 cat catalog/deployment.yml
 # Enter secret and image name into yaml
 sed -i.bak s%binding-compose-for-elasticsearch%${elastic_secret}%g catalog/deployment.yml
-sed -i.bak s%registry.ng.bluemix.net/chrisking/catalog:v1%${FULL_REPOSITORY_NAME}%g catalog/deployment.yml
+sed -i.bak s%registry.ng.bluemix.net/chrisking/catalog:v1%registry.ng.bluemix.net/chrisking/catalog:${env.BUILD_NUMBER}%g catalog/deployment.yml
 cat catalog/deployment.yml
 
 # Delete previous service
+# Do rolling update here
 echo -e "Deleting previous version of catalog if it exists"
-kubectl delete --ignore-not-found=true -f catalog/service.yml
-kubectl delete --ignore-not-found=true -f catalog/deployment.yml
+kubectl --token=${token} delete --ignore-not-found=true -f catalog/service.yml
+kubectl --token=${token} delete --ignore-not-found=true -f catalog/deployment.yml
 
 # Deploy service
 echo -e "Creating pods"
-kubectl create -f catalog/deployment.yml
-kubectl create -f catalog/service.yml
+kubectl --token=${token} create -f catalog/deployment.yml
+kubectl --token=${token} create -f catalog/service.yml
 
-PORT=$(kubectl get services | grep frontend | sed 's/.*://g' | sed 's/\/.*//g')
+PORT=$(kubectl --token=${token} get services | grep frontend | sed 's/.*://g' | sed 's/\/.*//g')
 
 echo "View the guestbook at http://$IP_ADDR:$PORT"
