42e9c5a0e05c45178058785969c4f4ce0d702899
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-03 16:14:46 -0500

    Added stage to cleanup

--- inventory/scripts/deploy.sh
+++ inventory/scripts/deploy.sh
@@ -12,20 +12,12 @@ sed -i.bak s%${string_to_replace}%${build_number}%g values.yaml
 # Install/Upgrade Chart
 cd ..
 
-release=$(helm list | grep inventory | awk '{print \$1}' | head -1)
+release=$(helm list | grep inventory | awk '{print $1}' | head -1)
 
-if [[ -z \"${release// }\" ]]; then
+if [[ -z "${release// }" ]]; then
     echo "Installing inventory chart for the first time"
     helm install bc-inventory
 else
     echo "Upgrading inventory chart release"
-    helm upgrade \${release} bc-inventory
+    helm upgrade ${release} bc-inventory
 fi
-
-
-# Delete all helm jobs for this chart
-cd bc-inventory
-chart_name=$(yaml read Chart.yaml name)
-chart_version=$(yaml read Chart.yaml version)
-
-helm delete jobs -l chart=${chart_name}-${chart_version}
