acc5bd1da84fffa289cf6f28465bf61c78a63b50
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-03 20:19:28 -0500

    - Renamed chart again - Made deploy scripts more configurable for registry name and namespace

--- inventory/scripts/deploy.sh
+++ inventory/scripts/deploy.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -x
 
-build_number=$1
+BUILD_NUMBER=$1
+REGISTRY_NAME=$2
+REGISTRY_NAMESPACE=$3
+
+# Init helm
 helm init
 
 # Edit chart values using yaml (NEED TO INSTALL YAML) - Call image chart deployer
-cd ../chart/bc-inventory
+cd ../chart/inventory
+
+# Replace tag
 string_to_replace=$(yaml read values.yaml image.tag)
-sed -i.bak s%${string_to_replace}%${build_number}%g values.yaml
+sed -i.bak s%${string_to_replace}%${BUILD_NUMBER}%g values.yaml
+
+# Replace registry name
+string_to_replace=$(yaml read values.yaml registry.name)
+sed -i.bak s%${string_to_replace}%${REGISTRY_NAME}%g values.yaml
+
+# Replace registry namespace
+string_to_replace=$(yaml read values.yaml registry.namespace)
+sed -i.bak s%${string_to_replace}%${REGISTRY_NAMESPACE}%g values.yaml
 
 # Install/Upgrade Chart
 cd ..
@@ -16,8 +30,8 @@ release=$(helm list | grep inventory | awk '{print $1}' | head -1)
 
 if [[ -z "${release// }" ]]; then
     echo "Installing inventory chart for the first time"
-    helm install bc-inventory
+    helm install inventory
 else
     echo "Upgrading inventory chart release"
-    helm upgrade ${release} bc-inventory
+    helm upgrade ${release} inventory
 fi
