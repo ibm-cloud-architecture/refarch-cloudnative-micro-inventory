3269fc796eefe03da4f45b972648a96d9026ea33
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-05 14:19:22 -0500

    New image notation

--- catalog/scripts/deploy.sh
+++ catalog/scripts/deploy.sh
@@ -9,19 +9,15 @@ REGISTRY_NAMESPACE=$3
 helm init
 
 # Edit chart values using yaml (NEED TO INSTALL YAML) - Call image chart deployer
-cd ../chart/catalog
+cd ../chart/bluecompute-catalog
 
 # Replace tag
 string_to_replace=$(yaml read values.yaml image.tag)
 sed -i.bak s%${string_to_replace}%${BUILD_NUMBER}%g values.yaml
 
-# Replace registry name
-string_to_replace=$(yaml read values.yaml registry.name)
-sed -i.bak s%${string_to_replace}%${REGISTRY_NAME}%g values.yaml
-
-# Replace registry namespace
-string_to_replace=$(yaml read values.yaml registry.namespace)
-sed -i.bak s%${string_to_replace}%${REGISTRY_NAMESPACE}%g values.yaml
+# Replace image repository
+string_to_replace=$(yaml read values.yaml image.repository)
+sed -i.bak s%${string_to_replace}%${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/bluecompute-catalog%g values.yaml
 
 # Install/Upgrade Chart
 cd ..
@@ -30,8 +26,8 @@ release=$(helm list | grep catalog | awk '{print $1}' | head -1)
 
 if [[ -z "${release// }" ]]; then
     echo "Installing catalog chart for the first time"
-    helm install catalog
+    helm install bluecompute-catalog
 else
     echo "Upgrading catalog chart release"
-    helm upgrade ${release} catalog
+    helm upgrade ${release} bluecompute-catalog
 fi
--- inventory/scripts/deploy.sh
+++ inventory/scripts/deploy.sh
@@ -9,19 +9,15 @@ REGISTRY_NAMESPACE=$3
 helm init
 
 # Edit chart values using yaml (NEED TO INSTALL YAML) - Call image chart deployer
-cd ../chart/inventory
+cd ../chart/bluecompute-inventory
 
 # Replace tag
 string_to_replace=$(yaml read values.yaml image.tag)
 sed -i.bak s%${string_to_replace}%${BUILD_NUMBER}%g values.yaml
 
-# Replace registry name
-string_to_replace=$(yaml read values.yaml registry.name)
-sed -i.bak s%${string_to_replace}%${REGISTRY_NAME}%g values.yaml
-
-# Replace registry namespace
-string_to_replace=$(yaml read values.yaml registry.namespace)
-sed -i.bak s%${string_to_replace}%${REGISTRY_NAMESPACE}%g values.yaml
+# Replace image repository
+string_to_replace=$(yaml read values.yaml image.repository)
+sed -i.bak s%${string_to_replace}%${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/bluecompute-inventory%g values.yaml
 
 # Install/Upgrade Chart
 cd ..
@@ -30,8 +26,8 @@ release=$(helm list | grep inventory | awk '{print $1}' | head -1)
 
 if [[ -z "${release// }" ]]; then
     echo "Installing inventory chart for the first time"
-    helm install inventory
+    helm install bluecompute-inventory
 else
     echo "Upgrading inventory chart release"
-    helm upgrade ${release} inventory
+    helm upgrade ${release} bluecompute-inventory
 fi
