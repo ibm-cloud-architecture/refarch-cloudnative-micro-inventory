bed87b699a40464c0e677a4ee591d672bf150d6a
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-18 13:25:30 -0500

    Updated pipelines

--- catalog/scripts/deploy.sh
+++ catalog/scripts/deploy.sh
@@ -2,41 +2,41 @@
 set -x
 
 BUILD_NUMBER=$1
-REGISTRY_NAME=$2
-REGISTRY_NAMESPACE=$3
-ORG=$4
-SPACE=$5
+BX_REGISTRY=$2
+BX_CR_NAMESPACE=$3
+BX_ORG=$4
+BX_SPACE=$5
 CLUSTER_NAME=$6
-API_KEY=$7
+BX_API_KEY=$7
 
 # Init helm
 helm init
 
-# Edit chart values using yaml (NEED TO INSTALL YAML) - Call image chart deployer
+# Install/Upgrade Chart
 cd ../chart/bluecompute-catalog
 
-# Install/Upgrade Chart
-release=$(helm list | grep bluecompute-catalog | awk '{print $1}' | head -1)
+# Replace values
+cat values.yaml | \
+    yaml w - image.tag ${BUILD_NUMBER} | \
+    yaml w - image.repository ${BX_REGISTRY}/${BX_CR_NAMESPACE}/bluecompute-catalog | \
+    yaml w - hs256key.skipDelete true | \
+    yaml w - configMap.skipDelete true | \
+    yaml w - secret.skipDelete true | \
+    yaml w - configMap.bluemixOrg ${BX_ORG} | \
+    yaml w - configMap.bluemixSpace ${BX_SPACE} | \
+    yaml w - configMap.bluemixRegistryNamespace ${BX_CR_NAMESPACE} | \
+    yaml w - configMap.kubeClusterName ${CLUSTER_NAME} | \
+    yaml w - secret.apiKey ${BX_API_KEY} > \
+        values_new.yaml
+
+mv values_new.yaml values.yaml
+
+release=$(helm list | grep catalog | awk '{print $1}' | head -1)
 
 if [[ -z "${release// }" ]]; then
     echo "Installing bluecompute-catalog chart for the first time"
-    time helm install --name catalog \
-    --set image.tag=${BUILD_NUMBER} \
-    --set image.repository=${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/bluecompute-catalog \
-	--set configMap.bluemixOrg=${ORG} \
-	--set configMap.bluemixSpace=${SPACE} \
-	--set configMap.kubeClusterName=${CLUSTER_NAME} \
-	--set secret.apiKey=${API_KEY} \
-	. --debug --wait --timeout 600
-
+    time helm install --name catalog . --debug --wait --timeout 600
 else
     echo "Upgrading bluecompute-catalog chart release"
-    time helm upgrade catalog \
-    --set image.tag=${BUILD_NUMBER} \
-    --set image.repository=${REGISTRY_NAME}/${REGISTRY_NAMESPACE}/bluecompute-catalog \
-	--set configMap.bluemixOrg=${ORG} \
-	--set configMap.bluemixSpace=${SPACE} \
-	--set configMap.kubeClusterName=${CLUSTER_NAME} \
-	--set secret.apiKey=${API_KEY} \
-	. --debug --wait --timeout 600
+    time helm upgrade catalog . --debug --wait --timeout 600
 fi
