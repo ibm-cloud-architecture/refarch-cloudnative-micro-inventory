304b47c3b4f82703a6323773cdd7b2b406ce6500
jkwong888 <jkwong@ca.ibm.com>
2017-07-17 17:39:03 +0800

    base64 decode secrets before using them

--- catalog/docker/startup.sh
+++ catalog/docker/startup.sh
@@ -77,7 +77,7 @@ if [ -z ${elastic+x} ]; then
     echo "Secret not in \"elastic\" variable. Probably NOT running in Kubernetes";
 else 
 	echo "Running in Kubernetes";
-    el_uri=$(echo $elastic | jq .uri | sed s%\"%%g)
+    el_uri=$(echo $elastic | base64 -d | jq -r .uri)
 
 	# Do the URL parsing
 	uri_parser $el_uri
