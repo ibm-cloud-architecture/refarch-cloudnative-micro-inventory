8a1751ce57d22fa09c33fc1bf459af19b696d282
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-01-29 14:10:19 -0600

    Removing unused code

--- catalog/src/main/java/application/rest/JaxrsApplication.java
+++ catalog/src/main/java/application/rest/JaxrsApplication.java
@@ -3,12 +3,8 @@ package application.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ApplicationPath("/micro")
 public class JaxrsApplication extends Application {
-	private static final Logger logger = LoggerFactory.getLogger(ElasticSearch.class);
-	
 
 }
