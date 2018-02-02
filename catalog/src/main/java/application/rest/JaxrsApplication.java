0bd08a57ead0c4a4a2f0a1c743b0005b774d55d6
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-02-02 15:56:40 -0600

    Cache refresh

--- catalog/src/main/java/application/rest/JaxrsApplication.java
+++ catalog/src/main/java/application/rest/JaxrsApplication.java
@@ -1,10 +1,21 @@
 package application.rest;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
 @ApplicationPath("/micro")
+@Singleton
+@Startup
 public class JaxrsApplication extends Application {
+	
+	@PostConstruct
+    public void init() {
+		InventoryRefreshTask inv = new InventoryRefreshTask();
+        inv.start();
+    }
 
 }
