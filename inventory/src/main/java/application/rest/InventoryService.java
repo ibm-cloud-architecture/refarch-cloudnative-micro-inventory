f8c7a3b984956bad8bb65d23723ce3d1702a93c1
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-03-08 16:53:02 -0500

    Enabling mq consumer

--- inventory/src/main/java/application/rest/InventoryService.java
+++ inventory/src/main/java/application/rest/InventoryService.java
@@ -1,6 +1,8 @@
 package application.rest;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,16 +10,44 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.google.gson.Gson;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 
 import utils.InventoryDAOImpl;
 
 
 @Path("/inv")
 public class InventoryService {
+	
+	private final static String QUEUE_NAME = "hello";
+	
 	@GET
 	 @Path("/check")
 	 @Produces("application/json")
-	 public String check() {
+	 public String check() throws IOException, TimeoutException {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+	    
+	    DefaultConsumer consumer = new DefaultConsumer(channel) {
+	        @Override
+	        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+	            throws IOException {
+	          String message = new String(body, "UTF-8");
+	          System.out.println(" [x] Received '" + message + "'");
+	        }
+	      };
+	      channel.basicConsume(QUEUE_NAME, true, consumer);
 	 return "it works!";
 	}
 	
