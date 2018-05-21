e80ccd19eb1ae872bae414518ea69115b5bd1e32
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-05-21 15:50:05 -0500

    RabbitMQ health check

--- inventory/src/main/java/application/rest/HealthEndpoint.java
+++ inventory/src/main/java/application/rest/HealthEndpoint.java
@@ -1,43 +1,111 @@
 package application.rest;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import javax.enterprise.context.ApplicationScoped;
 
-
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 
 @Health
 @ApplicationScoped
 
 public class HealthEndpoint implements HealthCheck {
 
+	private final static String QUEUE_NAME = "health";
+	String health = "health_check";
+
 	public boolean isInventoryDbReady(){
 		//Check if db is available
 		return true;
 	}
-	
-	public boolean isRabbitMQReady() {
-	    // check if rabbit is available?
-		return true;
-	  }
-	
+
+	public boolean isRabbitMQReady() throws IOException, TimeoutException {
+
+		boolean msgStatus = sendMessage();
+
+		if(msgStatus == true){
+			return true;
+		}
+		return false;
+
+	}
+
+	public boolean sendMessage(){
+		try
+		{
+			ConnectionFactory factory = new ConnectionFactory();
+			Config config = ConfigProvider.getConfig();
+			String rabbit_host = config.getValue("rabbit", String.class);
+			String sentMsg = null;
+			factory.setHost(rabbit_host);
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+			channel.basicPublish("", QUEUE_NAME, null, health.getBytes());
+			System.out.println("Sent the message '" + health + "'");
+
+			boolean autoAck = true;
+			GetResponse response = channel.basicGet(QUEUE_NAME, autoAck);
+			if (response == null) {
+				// No message retrieved.
+			}
+			else
+			{
+				byte[] body = response.getBody();
+				sentMsg = new String(body);
+			}
+
+			if(sentMsg.equals(health)){
+				return true;
+			}
+
+			channel.close();
+			connection.close();
+			return false;
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		catch(TimeoutException e){
+			e.printStackTrace();
+			return false;
+		}
+		}
+
 	@Override
 	public HealthCheckResponse call() {
 		// TODO Auto-generated method stub
-		
+
 		if (!isInventoryDbReady()) {
 		      return HealthCheckResponse.named(InventoryService.class.getSimpleName())
 		                                .withData("Inventory Database", "DOWN").down()
 		                                .build();
 		    }
-		
-		if (!isRabbitMQReady()) {
-		      return HealthCheckResponse.named(InventoryService.class.getSimpleName())
-		                                .withData("RabbitMQ", "DOWN").down()
-		                                .build();
-		    }
-		return HealthCheckResponse.named(InventoryService.class.getSimpleName()).up().build();
+
+		try {
+			if (!isRabbitMQReady()) {
+			      return HealthCheckResponse.named(InventoryService.class.getSimpleName())
+			                                .withData("RabbitMQ", "DOWN").down()
+			                                .build();
+			    }
+		} catch (IOException | TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return HealthCheckResponse.named(InventoryService.class.getSimpleName()).withData("Inventory Service", "UP").up().build();
 	}
 
 }
