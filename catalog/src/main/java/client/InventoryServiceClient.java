2bdb048a8c7ad9093714386503bc697b6f36b9d1
dshimo <dshi@utexas.edu>
2018-05-29 14:27:49 -0500

    removed unnecessary imports from InventoryServiceClient

--- catalog/src/main/java/client/InventoryServiceClient.java
+++ catalog/src/main/java/client/InventoryServiceClient.java
@@ -3,9 +3,7 @@ package client;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.enterprise.context.Dependent;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
