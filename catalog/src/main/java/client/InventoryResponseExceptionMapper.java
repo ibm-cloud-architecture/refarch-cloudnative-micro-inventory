6fe3b4d3aa1b6d917e54a0ef6ea3450b5f775bb7
dshimo <dshi@utexas.edu>
2018-06-04 11:59:19 -0500

    503 is supposed to be 500 from Inventory.

--- catalog/src/main/java/client/InventoryResponseExceptionMapper.java
+++ catalog/src/main/java/client/InventoryResponseExceptionMapper.java
@@ -4,20 +4,24 @@ import org.eclipse.microprofile.rest.client.ext.ResponseExceptionMapper;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+import java.util.logging.Logger;
 
 public class InventoryResponseExceptionMapper implements ResponseExceptionMapper<BaseInventoryException>{
 
+    Logger LOG = Logger.getLogger(BaseInventoryException.class.getName());
+
     @Override
     public boolean handles(int statusCode, MultivaluedMap<String, Object> headers) {
+        LOG.info("status = " + statusCode);
         return statusCode == 404    // Not Found
-            || statusCode == 503;   // Service Not Available
+            || statusCode == 500;   // Generic Server Error (Service Not Available)
     }
 
     @Override
     public BaseInventoryException toThrowable(Response response) {
         switch(response.getStatus()) {
             case 404: return new UnknownUrlException();
-            case 503: return new ServiceNotReadyException();
+            case 500: return new ServiceNotReadyException();
         }
         return null;
     }
