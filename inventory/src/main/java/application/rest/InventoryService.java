7a9eb345a06a2000f2bf282b8947f849750680df
dshimo <dshi@utexas.edu>
2018-06-21 10:20:48 -0500

    some formatting and a typo

--- inventory/src/main/java/application/rest/InventoryService.java
+++ inventory/src/main/java/application/rest/InventoryService.java
@@ -39,14 +39,15 @@ import utils.InventoryDAOImpl;
 @ApplicationScoped
 @Path("/inventory")
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Inventory Service", 
-				version = "0.0", 
-				description = "Inventory APIs",
-				contact = @Contact(url = "https://github.com/ibm-cloud-architecture", name = "IBM CASE"),
-				license = @License(name = "License", url = "https://github.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory/blob/microprofile/inventory/LICENSE")
-				)
-		)
+        info = @Info(
+                title = "Inventory Service",
+                version = "0.0",
+                description = "Inventory APIs",
+                contact = @Contact(url = "https://github.com/ibm-cloud-architecture", name = "IBM CASE"),
+                license = @License(name = "License",
+                        url = "https://github.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory/blob/microprofile/inventory/LICENSE")
+        )
+)
 public class InventoryService {
 
     private final static String QUEUE_NAME = "stock";
@@ -54,51 +55,50 @@ public class InventoryService {
     @GET
     @Produces("application/json")
     @APIResponses(value = {
-            @APIResponse( 
-            		responseCode = "404", 
-            		description = "Inventory Not Found", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		),
-            @APIResponse( 
-            		responseCode = "500", 
-            		description = "Internal Server Error", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		),
-            @APIResponse( 
-            		responseCode = "200",
-            		description = "List of items from the Inventory", 
-            		content = @Content( 
-            				mediaType = "application/json", 
-            				schema = @Schema(implementation = Inventory.class)
-            				)
-            		)
-            }
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Inventory Not Found",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            ),
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            ),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "List of items from the Inventory",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Inventory.class)
+                    )
+            )
+    }
     )
-    @Operation( 
-    		summary = "Get Inventory Items", 
-    		description = "Retriving all the available items from the inventory database"
-    		)
-    @Timed(name = "Inventory.timer", 
-	   	   absolute = true,
-	   	   displayName="Inventory Timer",
-	   	   description = "Time taken by the Inventory",
-	   	   reusable=true)
-    @Counted(name="Inventory",
-		     absolute = true,
-		     displayName="Inventory Call count", 
-		     description="Number of times the Inventory call happened.", 
-		     monotonic=true,
-		     reusable=true)
-    @Metered(name="InventoryMeter",
-		     displayName="Inventory Call Frequency", 
-		     description="Rate of the calls made to Inventory",
-		     reusable=true)
+    @Operation(
+            summary = "Get Inventory Items",
+            description = "Retrieving all the available items from the inventory database"
+    )
+    @Timed (name = "Inventory.timer",
+            absolute = true,
+            displayName = "Inventory Timer",
+            description = "Time taken by the Inventory",
+            reusable = true)
+    @Counted(name = "Inventory",
+            absolute = true,
+            displayName = "Inventory Call count",
+            description = "Number of times the Inventory call happened.",
+            monotonic = true,
+            reusable = true)
+    @Metered(name = "InventoryMeter",
+            displayName = "Inventory Call Frequency",
+            description = "Rate of the calls made to Inventory",
+            reusable = true)
     public String getInvDetails() {
-        
         String invDetails = null;
         List invlist = null;
         InventoryDAOImpl inv = new InventoryDAOImpl();
@@ -115,26 +115,26 @@ public class InventoryService {
     @Path("/stock")
     @Produces("text/plain")
     @APIResponses(value = {
-            @APIResponse( 
-            		responseCode = "500", 
-            		description = "Internal Server Error", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		),
-            @APIResponse( 
-            		responseCode = "200",
-            		description = "Stock Validation", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		)
-            }
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            ),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Stock Validation",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            )
+    }
     )
-    @Operation( 
-    		summary = "Stock Validation", 
-    		description = "Validates the Inventory Stock"
-    		)
+    @Operation(
+            summary = "Stock Validation",
+            description = "Validates the Inventory Stock"
+    )
     public String stock() throws IOException, TimeoutException {
         consumer();
         return "Stock Validated";
