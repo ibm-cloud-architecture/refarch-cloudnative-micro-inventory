5fcd0a150885e8e3d413a8b788473fb5dc86228f
dshimo <dshi@utexas.edu>
2018-06-21 10:37:04 -0500

    a typo

--- catalog/src/main/java/application/rest/CatalogService.java
+++ catalog/src/main/java/application/rest/CatalogService.java
@@ -35,73 +35,73 @@ import org.eclipse.microprofile.openapi.annotations.media.Content;
 @Path("/items")
 @Produces(MediaType.APPLICATION_JSON)
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Catalog Service", 
-				version = "0.0", 
-				description = "Catalog APIs",
-				contact = @Contact(url = "https://github.com/ibm-cloud-architecture", name = "IBM CASE"),
-				license = @License(name = "License", url = "https://github.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory/blob/microprofile/catalog/LICENSE")
-				)
-		)
+        info = @Info(
+                title = "Catalog Service",
+                version = "0.0",
+                description = "Catalog APIs",
+                contact = @Contact(url = "https://github.com/ibm-cloud-architecture", name = "IBM CASE"),
+                license = @License(name = "License", url = "https://github.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory/blob/microprofile/catalog/LICENSE")
+        )
+)
 public class CatalogService {
 
     @Inject
-    ItemService itemsRepo; 
-    
+    ItemService itemsRepo;
+
     List<Item> list = null;
-    
+
     //private Config config = ConfigProvider.getConfig();
 
     //private boolean ft_enabled = config.getValue("fault_tolerance_enabled", Boolean.class);
 
     @Timeout(value = 2, unit = ChronoUnit.SECONDS)
-    @Retry(maxRetries = 2, maxDuration= 2000)
+    @Retry(maxRetries = 2, maxDuration = 2000)
     @Fallback(fallbackMethod = "fallbackInventory")
     @GET
     @APIResponses(value = {
-            @APIResponse( 
-            		responseCode = "404", 
-            		description = "Items Not Found", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		),
-            @APIResponse( 
-            		responseCode = "500", 
-            		description = "Internal Server Error", 
-            		content = @Content( 
-            				mediaType = "text/plain"
-            				)
-            		),
-            @APIResponse( 
-            		responseCode = "200",
-            		description = "List of items from the catalog", 
-            		content = @Content( 
-            				mediaType = "application/json", 
-            				schema = @Schema(implementation = Item.class)
-            				)
-            		)
-            }
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Items Not Found",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            ),
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content(
+                            mediaType = "text/plain"
+                    )
+            ),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "List of items from the catalog",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Item.class)
+                    )
+            )
+    }
     )
-    @Operation( 
-    		summary = "Get Inventory Items", 
-    		description = "Retriving all the available items from the cache"
-    		)
+    @Operation(
+            summary = "Get Inventory Items",
+            description = "Retrieving all the available items from the cache"
+    )
     public List<Item> getInventory() {
-    	List<Item> items = null;
-    	items = itemsRepo.findAll();
-		return items;
+        List<Item> items = null;
+        items = itemsRepo.findAll();
+        return items;
     }
 
     public List<Item> fallbackInventory() {
-    	
-    	//Returns a default fallback list
-    	List<Item> list = new ArrayList<Item>();
-        Item item = new Item("Standard fallback message","Standard fallback message",0,"Standard fallback message","Catalog-fallback.jpg",0);
+
+        //Returns a default fallback list
+        List<Item> list = new ArrayList<Item>();
+        Item item = new Item("Standard fallback message", "Standard fallback message", 0, "Standard fallback message", "Catalog-fallback.jpg", 0);
         list.add(item);
 
         return list;
-    	
+
         //Returns emptylist
         //return Collections.emptyList();
     }
@@ -109,30 +109,30 @@ public class CatalogService {
     @GET
     @Path("{id}")
     @APIResponses(value = {
-            @APIResponse( 
-            		responseCode = "404",
-            		description = "Item Not Found", 
-            		content = @Content( mediaType = "text/plain")
-            		),
             @APIResponse(
-            		responseCode = "500",
-            		description = "Internal Server Error",
-            		content = @Content( mediaType = "text/plain")
-            		),
-            @APIResponse( 
-            		responseCode = "200", 
-            		description = "Item retrieved by id", 
-            		content = @Content( 
-            				mediaType = "application/json", 
-            				schema = @Schema(implementation = Item.class)
-            				)
-            		)
-            }
+                    responseCode = "404",
+                    description = "Item Not Found",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content(mediaType = "text/plain")
+            ),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Item retrieved by id",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Item.class)
+                    )
+            )
+    }
     )
     @Operation(
-    		summary = "Get Inventory Items by Id", 
-    		description = "Retrieving the item from cache based on id"
-    		)
+            summary = "Get Inventory Items by Id",
+            description = "Retrieving the item from cache based on id"
+    )
     public Response getById(@Parameter(description = "The id of the item that needs to be fetched. For testing, use 13401", required = true) @PathParam("id") long id) {
         final Item item = itemsRepo.findById(id);
         if (item == null) {
