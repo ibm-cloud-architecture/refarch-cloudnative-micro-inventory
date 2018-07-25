18914250785faad240da1dd1aa664cfea65c754f
Fabio Gomez <fabiogomez@us.ibm.com>
2018-07-24 22:12:44 -0500

    - Ignore .bak files - Support for passwordless login - Replace database name in mysql_data.sql prior to loading data

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 mysql_user=$1
 mysql_password=$2
@@ -9,6 +9,12 @@ mysql_database=$5
 usage () {
 	printf "USAGE:\n\tbash load-data.sh USER PASSWORD HOST PORT DATABASE\n\n"
 	exit 1
+}
+
+wait_mysql () {
+	printf "Waiting for MySQL to fully initialize\n\n"
+	sleep 1
+    echo "trying to load data again"
 }
 
 # User
@@ -25,11 +31,6 @@ fi
 if [[ -z "$mysql_password" ]]; then
 	echo "Password not provided. Attempting container environment variable..."
 	mysql_password=$MYSQL_PASSWORD
-fi
-
-if [[ -z "$mysql_password" ]]; then
-	echo "Unable to get password."
-	usage
 fi
 
 # Optional
@@ -53,14 +54,28 @@ if [[ -z "$mysql_database" ]]; then
 	mysql_database='inventorydb'
 fi
 
-# load data
-while !(mysql -u${mysql_user} -p${mysql_password} --host ${mysql_host} --port ${mysql_port} <mysql_data.sql)
-do
-  printf "Waiting for MySQL to fully initialize\n\n"
-  sleep 1
-    echo "trying to load data again"
-done
+# Replacing database name in mysql_data.sql
+echo "Replacing database name in mysql_data.sql"
+if [[ $OSTYPE =~ .*darwin.* ]]; then
+	sed -i.bak "s/inventorydb/${mysql_database}/g" mysql_data.sql
+else
+	sed -i "s/inventorydb/${mysql_database}/g" mysql_data.sql
+fi
 
-#rm load-data.sql testdata
+# load data
+echo "Loading data..."
+if [[ -z "$mysql_password" ]]; then
+	echo "No password set. Probably using passwordless root"
+	while !(mysql -u${mysql_user} --host ${mysql_host} --port ${mysql_port} <mysql_data.sql)
+	do
+		wait_mysql
+	done
+else
+	echo "Password set"
+	while !(mysql -u${mysql_user} -p${mysql_password} --host ${mysql_host} --port ${mysql_port} <mysql_data.sql)
+	do
+		wait_mysql
+	done
+fi
+
 printf "\n\nData loaded to %s.items.\n\n" "${mysql_database}"
-exit 0
