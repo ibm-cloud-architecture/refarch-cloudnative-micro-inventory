535d5944f699506eeb76f7903aa162bd20f87a11
Fabio Gomez <fabiogomez@us.ibm.com>
2018-12-12 08:45:09 -0600

    - Renaming variables to avoid conflicts with pod environment variables

--- scripts/api_tests.sh
+++ scripts/api_tests.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 function parse_arguments() {
-	# INVENTORY_HOST
-	if [ -z "${INVENTORY_HOST}" ]; then
-		echo "INVENTORY_HOST not set. Using parameter \"$1\"";
-		INVENTORY_HOST=$1;
+	# MICROSERVICE_HOST
+	if [ -z "${MICROSERVICE_HOST}" ]; then
+		echo "MICROSERVICE_HOST not set. Using parameter \"$1\"";
+		MICROSERVICE_HOST=$1;
 	fi
 
-	if [ -z "${INVENTORY_HOST}" ]; then
-		echo "INVENTORY_HOST not set. Using default key";
-		INVENTORY_HOST=127.0.0.1;
+	if [ -z "${MICROSERVICE_HOST}" ]; then
+		echo "MICROSERVICE_HOST not set. Using default key";
+		MICROSERVICE_HOST=127.0.0.1;
 	fi
 
-	# INVENTORY_PORT
-	if [ -z "${INVENTORY_PORT}" ]; then
-		echo "INVENTORY_PORT not set. Using parameter \"$2\"";
-		INVENTORY_PORT=$2;
+	# MICROSERVICE_PORT
+	if [ -z "${MICROSERVICE_PORT}" ]; then
+		echo "MICROSERVICE_PORT not set. Using parameter \"$2\"";
+		MICROSERVICE_PORT=$2;
 	fi
 
-	if [ -z "${INVENTORY_PORT}" ]; then
-		echo "INVENTORY_PORT not set. Using default key";
-		INVENTORY_PORT=8080;
+	if [ -z "${MICROSERVICE_PORT}" ]; then
+		echo "MICROSERVICE_PORT not set. Using default key";
+		MICROSERVICE_PORT=8080;
 	fi
 
-	echo "Using http://${INVENTORY_HOST}:${INVENTORY_PORT}"
+	echo "Using http://${MICROSERVICE_HOST}:${MICROSERVICE_PORT}"
 }
 
 function get_inventory() {
-	CURL=$(curl -s --max-time 5 http://${INVENTORY_HOST}:${INVENTORY_PORT}/micro/inventory | jq '. | length');
+	CURL=$(curl -s --max-time 5 http://${MICROSERVICE_HOST}:${MICROSERVICE_PORT}/micro/inventory | jq '. | length');
 	echo "Found inventory with \"${CURL}\" items"
 
 	if [ -z "${CURL}" ] || [ ! "${CURL}" -gt "0" ]; then
